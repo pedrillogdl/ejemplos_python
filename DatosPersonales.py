5a84a49d84765ae611a612297db2c1a08278f091
Pedro Cortes <jovenmzt20@hotmail.com>
2017-10-23 23:37:43 -0500

    Primer Archivo Datos Personales

---
+++ DatosPersonales.py
@@ -0,0 +1,12 @@
+Nombre=input("Por favor, podrias proporcionar tu nombre: ")
+Direccion=input("Por favor, podrias proporcionar tu Direccion: ")
+Telefono=input("Por favor, podrias proporcionar tu Telefono: ")
+
+ListaGeneral=[Nombre, Direccion, Telefono]
+print("Los Datos Personsales Proporcionados son: ")
+print("Nombre: " + ListaGeneral[0])
+print("Direccion: " + ListaGeneral[1])
+print("Telefono: " + ListaGeneral[2])
+
+
+
